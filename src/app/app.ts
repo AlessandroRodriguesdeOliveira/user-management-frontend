0f0ab52debeb1f4a161b32c3d8b2b8bd4f5b36af
Alessandro Rodrigues deOliveira <alessandro14736@gmail.com>
2026-01-12 11:39:13 -0300

    add user and admin component; custom the user-main component ; modified footer and nav color

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Nav } from './nav/nav';
 import { Footer } from "./footer/footer";
+import { UserMain } from './user-main/user-main';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, Footer],
+  imports: [Nav, Footer, UserMain],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
