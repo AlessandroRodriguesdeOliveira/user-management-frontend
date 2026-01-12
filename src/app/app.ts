c027dc959e69c039d8cc506eb734d4b9a879b8b0
Alessandro Rodrigues deOliveira <alessandro14736@gmail.com>
2026-01-12 10:38:54 -0300

    add nav e footer and customization them

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Nav } from './nav/nav';
+import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Nav, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
